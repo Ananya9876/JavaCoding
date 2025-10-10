6b930d133737ad5d59259d8daaf88fec057a27eb
Ananya Naik <ananyamnaik6@gmail.com>
2025-10-11 00:15:48 +0530

    Day 2 code

--- Day1/code.java
+++ Day1/code.java
@@ -1,15 +1,19 @@
 // Take user input for name and age and print “Hello <name>, you are <age> years old.”
 
-public class Day1{
+package Day1;
+
+public class code {
 
 
     public void work(String n, int a){
+
     System.out.println("Hello "+n+" , you are "+a+" years of old");
 }
     public static void main(String[] args) {
-        Day1 d=new Day1();
-        d.work("Ananya", 22);
+        code c=new code();
+        c.work("Ananya", 22);
 
     }
 }
+
 
---
+++ Day2/code.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class code {
+    public static void main(String[] args) {
+        int n=11;
+        for(int i=1;i<=10;i++){
+        System.out.println(i*n);
+    }
+}
+    }
+
