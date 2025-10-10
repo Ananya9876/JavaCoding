08eba8ee274c8e0e52eea7d92896553501ad3da8
Ananya Naik <ananyamnaik6@gmail.com>
2025-10-10 22:46:32 +0530

    Day 1 Code

---
+++ Day1.java
@@ -0,0 +1,15 @@
+// Take user input for name and age and print “Hello <name>, you are <age> years old.”
+
+public class Day1{
+
+
+    public void work(String n, int a){
+    System.out.println("Hello "+n+" , you are "+a+" years of old");
+}
+    public static void main(String[] args) {
+        Day1 d=new Day1();
+        d.work("Ananya", 22);
+
+    }
+}
+
