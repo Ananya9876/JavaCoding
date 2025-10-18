dbdd64415d890584b0418ce3dd390a604ef631af
Ananya Naik <ananyamnaik6@gmail.com>
2025-10-18 10:32:53 +0530

    Day 5 code

---
+++ Day5/code.java
@@ -0,0 +1,19 @@
+// Merge two arrays.
+
+
+package Day5;
+
+public class code {
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        int[] b={6,7,8,9};
+
+        for(int i=0;i<=a.length-1;i++){
+            System.out.print(a[i]+" ");
+        }
+    
+    for(int j=0;j<=b.length-1;j++){
+        System.out.print(b[j]+" ");
+    }
+    }
+}
